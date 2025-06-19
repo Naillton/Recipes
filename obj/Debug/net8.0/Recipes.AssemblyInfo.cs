bacf991ab82a006c45090f59a734de70107d6b52
Naillton <Nailton_junior@protonmail.com>
2025-06-19 20:21:39 -0300

    UserModel

--- obj/Debug/net8.0/Recipes.AssemblyInfo.cs
+++ obj/Debug/net8.0/Recipes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0bbc535b39a346ceb53959f65b63346ce0a3d70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
